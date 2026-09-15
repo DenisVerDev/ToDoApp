3a3d251274d397989fcf08e9df3ee4e3155d86ec
DenisVerDev <denisverepadev@gmail.com>
2026-09-15 17:19:22 +0300

    created simple ICategoriesRepository

---
+++ ToDoAPI/Data/Repositories/CategoriesRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using ToDoAPI.Data.Models;
+
+namespace ToDoAPI.Data.Repositories
+{
+    public class CategoriesRepository (ToDoDbContext _dbContext) : ICategoriesRepository
+    {
+        public async System.Threading.Tasks.Task AddCategoryAsync(Category category)
+        {
+            await _dbContext.Categories.AddAsync(category);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async System.Threading.Tasks.Task DeleteCategoryAsync(Category category)
+        {
+            _dbContext.Categories.Remove(category);
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task<Category?> FetchCategoryAsync(Expression<Func<Category, bool>> predicate)
+            => await _dbContext.Categories.Where(predicate).FirstOrDefaultAsync();
+
+        public async Task<List<Category>> FetchCategoriesAsync(Expression<Func<Category, bool>> predicate)
+            => await _dbContext.Categories.Where(predicate).ToListAsync();
+
+        public async Task<bool> AnyCategoryAsync(Expression<Func<Category, bool>> predicate)
+            => await _dbContext.Categories.AnyAsync(predicate);
+    }
+}
---
+++ ToDoAPI/Data/Repositories/ICategoriesRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using ToDoAPI.Data.Models;
+using Task = System.Threading.Tasks.Task;
+
+namespace ToDoAPI.Data.Repositories
+{
+    public interface ICategoriesRepository
+    {
+        Task AddCategoryAsync(Category category);
+
+        Task DeleteCategoryAsync(Category category);
+
+        Task<Category?> FetchCategoryAsync(Expression<Func<Category, bool>> predicate);
+
+        Task<List<Category>> FetchCategoriesAsync(Expression<Func<Category, bool>> predicate);
+
+        Task<bool> AnyCategoryAsync(Expression<Func<Category, bool>> predicate);
+    }
+}
