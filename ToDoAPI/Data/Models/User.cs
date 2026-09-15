0a4ab1ed4b5db5a9aba6844357eb2c56b2c5c0c8
DenisVerDev <denisverepadev@gmail.com>
2026-09-15 10:41:49 +0300

    Identity database and user

--- ToDoAPI/Data/Models/User.cs
+++ ToDoAPI/Data/Models/User.cs
@@ -1,6 +1,11 @@
-﻿namespace ToDoAPI.Data.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ToDoAPI.Data.Models
 {
-    public class User
+    public class User : IdentityUser
     {
+        public IEnumerable<Task> Tasks { get; set; } = new List<Task>(); // Navigation property for all user's tasks
+
+        public IEnumerable<Category> Categories { get; set; } = new List<Category>(); // Navigation property for all user's categories
     }
 }
---
+++ ToDoAPI/Data/ToDoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Data.Models;
+
+namespace ToDoAPI.Data
+{
+    public class ToDoDbContext : IdentityDbContext<User>
+    {
+        public DbSet<Models.Task> Tasks { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+            : base(options)
+        {}
+
+
+    }
+}
