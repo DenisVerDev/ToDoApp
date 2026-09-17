0766c81af8f91fed062fe0c20900311d9e81e2c4
DenisVerDev <denisverepadev@gmail.com>
2026-09-17 10:46:40 +0300

    finihsed Repositorires.Categories.AddTests

--- ToDoAPI.Tests/Repositories/Categories/AddTests.cs
+++ ToDoAPI.Tests/Repositories/Categories/AddTests.cs
@@ -102,7 +102,105 @@ namespace ToDoAPI.Tests.Repositories.Categories
 
         #region Add Multiple
 
-        
+        [Fact]
+        public async Task AddCategoriesAsync_FreshCategories_AddsToRepo()
+        {
+            // Arrange
+            using var dbContext = _fixture.CreateDbContext();
+            var repository = new CategoriesRepository(dbContext);
+
+            var authorId = dbContext.Users.First().Id;
+
+            var categories = new List<Category>();
+            for(int i = 0; i < 4; i++)
+            {
+                categories.Add(new Category
+                {
+                    Name = Guid.NewGuid().ToString(),
+                    Color = "FFFFFF",
+                    AuthorId = authorId
+                });
+            }
+
+            // Act
+            await repository.AddCategoriesAsync(categories);
+
+            // Assert
+            await Assert.AllAsync(categories, c => dbContext.Categories.ContainsAsync(c)); // here it looks by id
+        }
+
+        [Fact]
+        public async Task AddCategoriesAsync_DuplicateCategories_ThrowsDbUpdateException()
+        {
+            // Arrange
+            using var dbContext = _fixture.CreateDbContext();
+            var repository = new CategoriesRepository(dbContext);
+
+            var duplicate = await dbContext.Categories.ToListAsync();
+
+            // Act
+            var result = await Record.ExceptionAsync(() => repository.AddCategoriesAsync(duplicate));
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<DbUpdateException>(result);
+        }
+
+        [Fact]
+        public async Task AddCategoriesAsync_DiffNameAuthorSameId_ThrowsDbUpdateException() // tests unique constraint on Name and AuthorId
+        {
+            // Arrange
+            using var dbContext = _fixture.CreateDbContext();
+            var repository = new CategoriesRepository(dbContext);
+
+            var specialDuplicates = await dbContext.Categories.Select(c => new Category
+            {
+                Id = c.Id*100, // diff Id
+                Name = c.Name, // same Name
+                Color = c.Color,
+                AuthorId = c.AuthorId // same AuthorId
+            }).ToListAsync();
+
+
+            // Act
+            var result = await Record.ExceptionAsync(() => repository.AddCategoriesAsync(specialDuplicates));
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<DbUpdateException>(result);
+        }
+
+        [Fact]
+        public async Task AddCategoriesAsync_EmptyCategories_DoesNothing()
+        {
+            // Arrange
+            using var dbContext = _fixture.CreateDbContext();
+            var repository = new CategoriesRepository(dbContext);
+
+            var categoriesCount = await dbContext.Categories.CountAsync();
+
+            // Act
+            var result = await Record.ExceptionAsync(() => repository.AddCategoriesAsync(new List<Category>()));
+
+            // Assert
+            Assert.Null(result);
+            Assert.Equal(categoriesCount, await dbContext.Categories.CountAsync());
+        }
+
+        [Fact]
+        public async Task AddCategoriesAsync_NullCategories_ThrowsNullReferenceException()
+        {
+            // Arrange
+            using var dbContext = _fixture.CreateDbContext();
+            var repository = new CategoriesRepository(dbContext);
+
+            // Act
+            var result = await Record.ExceptionAsync(() => repository.AddCategoriesAsync(null));
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<NullReferenceException>(result);
+        }
 
         #endregion
     }
