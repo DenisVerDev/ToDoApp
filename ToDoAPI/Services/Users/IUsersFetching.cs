f08c5554fdcee91e23c79f8c6974ea36500b7ced
DenisVerDev <denisverepadev@gmail.com>
2026-09-22 16:28:30 +0300

    Users services interfaces

---
+++ ToDoAPI/Services/Users/IUsersFetching.cs
@@ -0,0 +1,11 @@
+﻿using ToDoAPI.Data.Models;
+
+namespace ToDoAPI.Services.Users
+{
+    public interface IUsersFetching
+    {
+        Task<ServiceResult<User>> FetchUserAsync(string authorId);
+
+        Task<ServiceResultStatus> VerifyUserAsync(string email, string password);
+    }
+}
---
+++ ToDoAPI/Services/Users/IUsersManagement.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoAPI.Services.Users
+{
+    public interface IUsersManagement
+    {
+        Task<ServiceResultStatus> CreateUserAsync(string email, string password);
+
+        Task<ServiceResultStatus> DeleteUserAsync(string userId);
+    }
+}
